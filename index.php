1291a1e26a5f44938e1615f4d4e67c1d206c1445
mihhran <mihranaslanyan2004@gmail.com>
2021-08-14 19:58:06 +0400

    hgfythrytry

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+//class A{
+//    public $first_name = '';
+//    public function __construct($first_name){
+//        $this->first_name = $first_name;
+//}
+//publiv
+//}
+//
+//class B extends A{
+//    public function __construct($age, $first_name)
+//    {
+////        parent::__construct($first_name);
+//    }
+//}
+//
+//$ob1 = new B(30, 'John');
+//echo $ob1
+
+//class A{
+//    public function welcome(){
+//        echo "Hello <br>";
+//    }
+//}
+//class B extends A{
+//    public function welcome()
+//{
+//    echo "Hi <br>";
+//}
+//}
+//$ob1 = new A();
+//$ob2 = new B();
+//$ob1->welcome();
+//$ob2->welcome();
+
+//class A{
+//    public static $x = 10;
+//    public function getX(){
+//        return self::$x;
+//    }
+//}
+
+//abstract class A{
+//    public function getUser(){
+//        return 'John Smith';
+//    }
+//    abstract public function getX($x);
+//
+//}
+//class B extends A{
+//    public  function getX($x){
+//        return $x;
+//    }
+//}
+//$x1 = new B();
+//echo $x1->getUser();
+
+
+
+?>
